1f6053f5944781d345b083fa6607c25d33b1e69c
joffinkoshy <146009100+joffinkoshy@users.noreply.github.com>
2026-06-30 19:34:05 +0530

    Add: find-the-town-judge - submission-1

---
+++ Data Structures & Algorithms/find-the-town-judge/submission-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        indegree=[0]*(n+1)
+        outdegree=[0]*(n+1)
+
+        for a,b in trust:
+            outdegree[a]+=1
+            indegree[b]+=1
+
+        for i in range(1,n+1):
+            if outdegree[i]==0 and indegree[i]==n-1:
+                return i
+
+        return -1
+
+        
+            
+
+
+        
+
+        
+        
